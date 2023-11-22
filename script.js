750f6f2c4811058206b0480c8d81052ad622a58d
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-22 18:09:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 var isDate = function (input) {        
   //   write your code here
 
-	input=parseInt(input);
-
-	if (typeof input === "object" && input !== null) 
+	if (typeof input === "object") 
 	{
 		return true;
     } 
