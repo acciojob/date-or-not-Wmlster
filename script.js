fcec061927066fee8e3030cf422d6c40641b8034
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-22 17:52:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
-var isDate = function (input) {
+var isDate = function (input) {   
   //   write your code here
+
+	let n=input.length;
+
+	if(n>10)
+		return false;
+	
+	let ok=1;
+
+	for(let i=0;i<4;i++)
+		{
+			if(input[i]>='0'&&input[i]<='9')
+			{
+				continue;
+			}
+			else
+			{
+				ok=0;
+				break;
+			}
+		}
+
+	if(input[4]!='-'||input[7]!='-')
+	{
+		ok=0;
+	}
+
+	for(let i=5;i<=6;i++)
+		{
+			if(input[i]>='0'&&input[i]<='9')
+			{
+				continue;
+			}
+			else
+			{
+				ok=0;
+				break;
+			}
+		}
+
+	for(let i=8;i<=9;i++)
+		{
+			if(input[i]>='0'&&input[i]<='9')
+			{
+				continue;
+			}
+			else
+			{
+				ok=0;
+				break;
+			}
+		}
+
+	if(ok==0)
+		return false;
+	return true;
 };
 
 // Do not change the code below.
